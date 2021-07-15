be01b24189112faa3771483080b0b98d9ad7b56a
RamRaid <71435969+junpyo319@users.noreply.github.com>
2021-07-15 09:24:12 +0900

    Update wordlist_gen.py

--- wordlist_gen.py
+++ wordlist_gen.py
@@ -1,47 +1,45 @@
-import re
 from itertools import product
 
-header = 'wordlist_gen..'
+print('''
+
+\033[93m☆\033[95m˚″´·.wordlist_gen..
+
+''')
 
 def get_len() :
-  len_opt = input('set length.. e.g. 4:8..\t')
-  return re.split(':',len_opt)
+  len_opt = input('1.\t\tset length.. e.g. 4:8..\t')
+  return len_opt.split(':')
 
 def get_chrset() :
-  chrset_opt1 = input('Do you want to include alphabet into the set? (y/n)\t')
-  if chrset_opt1 == 'y' :
-    chrset_opt1_1 = input('Do you want specify alphabet? (y/n)\t')
-    if chrset_opt1_1 == 'y' :
-      chrset_tmp1 = input('input char set.. e.g. abcd\t')
-    elif chrset_opt1_1 == 'n' :
-      chrset_tmp1 = 'abcdefghijklmnopqrstuvwxyz'
+  if input('2.\t\tDo you want to include alphabets into the set? (y/n)\t') == 'y' :
+    if input('2-1.\tDo you want specify alphabets? (y/n)\t') == 'y' :
+      chrset_tmp1 = input('\t\tinput alphabets.. e.g. abcd\t')
     else :
-      get_chrset()
-  elif chrset_opt1 == 'n' :
+      chrset_tmp1 = 'abcdefghijklmnopqrstuvwxyz'
+    if input('2-2.\tDo you want include upper alphabets? (y/n)\t') == 'y' :
+      chrset_tmp1 = ''.join([chrset_tmp1, chrset_tmp1.upper()])
+  else :
     chrset_tmp1 = ''
-  else :
-    get_chrset()
 
-  chrset_opt2 = input('Do you want to include numeric char into the set? (y/n)\t')
-  if chrset_opt2 == 'y' :
-    chrset_opt2_1 = input('Do you want to specify nemeric char? (y/n)\t')
-    if chrset_opt2_1 == 'y' :
-      chrset_tmp2 = input('input numeric set.. e.g. 1234\t')
-    elif chrset_opt2_1 == 'n' :
-      chrset_tmp2 = '123456789'
-  elif chrset_opt2 == 'n' :
+  if input('3.\t\tDo you want to include numeric chars into the set? (y/n)\t') == 'y' :
+    if input('3-1.\tDo you want to specify nemeric chars? (y/n)\t') :
+      chrset_tmp2 = input('\t\tinput numeric chars.. e.g. 1234\t')
+    else :
+      chrset_tmp2 = '0123456789'
+    if input('3-2.\tDo you want include special chars? (y/n)\t') == 'y' :
+      chrset_tmp2 = ''.join([chrset_tmp2, '!\][/?.,~-=";:><@#$%&*()_+\''])
+  else :
     chrset_tmp2 = ''
-  else :
-    get_chrset()
 
-  chrset_tmp = chrset_tmp1 + chrset_tmp2
-  print('chrset : ' + chrset_tmp)
+  chrset = ''.join([chrset_tmp1, chrset_tmp2])
+  
+  print('chrset : ' + chrset)
 
-  if not chrset_tmp :
+  if not chrset :
     print('chrset is empty...')
-    get_chrset()
-  elif chrset_tmp:
-    return [chr for chr in chrset_tmp]
+    return get_chrset()
+  elif chrset:
+    return [chr for chr in chrset]
 
 def print_list(len_opt, chrset) :
   i = int(len_opt[0])
@@ -60,8 +58,11 @@ def duple(chrset) :
         return duple(chrset)
       elif (i == chrset_len - 2) and (j == chrset_len - 1) :
         return chrset
+    
 
 len_opt = get_len()
 chrset = get_chrset()
 chrset = duple(chrset)
 print_list(len_opt,chrset)
+
+print('done...·´″˚\033[93m☆\033[95m')
